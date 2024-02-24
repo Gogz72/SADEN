406aa4ad41740f4e2b0d72128c4ecde029ccd84c
Gogz72 <gamalhazemmohmed@gmail.com>
2024-02-24 19:39:51 +0200

    did some adjustments to the lua code and to the highlevel planner now the target is moving and the drone is following

--- apf/scripts/high_level.py
+++ apf/scripts/high_level.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import numpy as np
 import math
 import rospy
@@ -5,17 +7,82 @@ from geometry_msgs.msg import Quaternion, PoseStamped, Pose, Point
 from nav_msgs.msg import Path
 from std_msgs.msg import Bool
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import time
+
+def set_current_pose(data):
+    global current_point, current_orientation
+
+    current_point = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+
+    current_orientation = np.array([data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w])
+   
+def set_goal_pose(data):
+    global goal_point, goal_orientation
+
+    goal_point = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
+
+    goal_orientation = np.array([data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w])
+
+def pi_2_pi(angle):
+    #A function to wrap the angle between -pi and pi (for numerical stability)
+    return (angle + np.pi) % (2 * np.pi) - np.pi
 
 
 def clock_wise (angle):
     angle = angle + (np.pi/2)
+    angle = pi_2_pi(angle)
     return angle
 
 def anti_clock_wise (angle):
     angle = angle - (np.pi/2)
+    angle = pi_2_pi(angle)
     return angle
 
 def one_eighty (angle):
     angle = angle + np.pi
+    angle = pi_2_pi(angle)
     return angle
+
+def set_point(x,y,z,x_th,y_th,z_th):
+
+    target_msg = PoseStamped()
+    target_msg.header.stamp = rospy.Time.now()
+    target_msg.pose.position.x = x
+    target_msg.pose.position.y = y
+    target_msg.pose.position.z = z  # Assuming z-coordinate is 0 for control point
+    target_msg.pose.orientation = Quaternion(*quaternion_from_euler(x_th, y_th, z_th))
+
+    target_pub.publish(target_msg)
+
+if __name__ == '__main__':
+    rospy.init_node("high_level")
+
+    sub_current_pose = rospy.Subscriber('/Pos', PoseStamped, set_current_pose)
+    sub_goal_pose = rospy.Subscriber('/Target_Pos', PoseStamped, set_goal_pose)
+    target_pub = rospy.Publisher('/Target_commands',PoseStamped, queue_size=10)
+
+    current_point = np.array([0.0, 0.0, 0.0])  # Initialize as numpy array
+    current_orientation = np.array([0.0, 0.0, 0.0, 0.0])
+    goal_point = np.array([0.0, 0.0, 0.0])  # Initialize as numpy array
+    goal_orientation = np.array([0.0, 0.0, 0.0, 0.0])
+
+    step_size = 1.0
+
+    step_size_time = 10.0
+
+    time.sleep(step_size_time)
+
+
+    while not rospy.is_shutdown():
+    
+     set_point(0.0,0.0,1.0,0.0,0.0,0.0)   
+
+     time.sleep(step_size_time)
+
+     set_point(-2.0,-2.0,1.0,0.0,0.0,0.0)   
+
+     time.sleep(step_size_time)
+
+
+
 
