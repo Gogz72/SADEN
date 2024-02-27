2e6bc60335f9c8b34c8de7840e5a5c5e17ce312f
Gogz <gamalhazemmohmed@gmail.com>
2024-02-27 19:37:47 +0200

    latest update still distance is not accurate

--- apf/scripts/high_level.py
+++ apf/scripts/high_level.py
@@ -5,7 +5,8 @@ import math
 import rospy
 from geometry_msgs.msg import Quaternion, PoseStamped, Pose, Point
 from nav_msgs.msg import Path
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, Float32
+from sensor_msgs.msg import Range
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import time
 import random
@@ -16,6 +17,22 @@ def set_current_pose(data):
     current_point = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
 
     current_orientation = np.array([data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w])
+
+
+def obstacle_coordinates(drone_x, drone_y, drone_orientation, distance):
+    # Convert sensor direction to angle relative to world frame
+    angle = drone_orientation 
+    # Convert angle to radians
+    # Calculate obstacle coordinates relative to world frame
+    obstacle_x = drone_x + distance * np.cos(angle)
+    obstacle_y = drone_y + distance * np.sin(angle)
+    return obstacle_x, obstacle_y
+
+def set_obstacle_distance (data):
+    global obstacle_length
+    obstacle_length = data
+    
+    
    
 def set_goal_pose(data):
     global goal_point, goal_orientation
@@ -68,11 +85,13 @@ if __name__ == '__main__':
     sub_current_pose = rospy.Subscriber('/Pos', PoseStamped, set_current_pose)
     sub_goal_pose = rospy.Subscriber('/Target_Pos', PoseStamped, set_goal_pose)
     target_pub = rospy.Publisher('/Target_commands',PoseStamped, queue_size=10)
+    obstacle_pub = rospy.Publisher('/obstacle_coord',PoseStamped,queue_size=10)
+    obstacle_sub = rospy.Subscriber('/obstacle',Float32,set_obstacle_distance)
 
     current_point = np.array([0.0, 0.0, 0.0])  # Initialize as numpy array
-    current_orientation = np.array([0.0, 0.0, 0.0, 0.0])
-    goal_point = np.array([0.0, 0.0, 0.0])  # Initialize as numpy array
-    goal_orientation = np.array([0.0, 0.0, 0.0, 0.0])
+    obstacle_length = -1.0
+    obstacle_coords = PoseStamped()
+
 
     x = 0.0
     y = 0.0
@@ -104,8 +123,9 @@ if __name__ == '__main__':
             
         start2_time = time.time()
         while time.time() - start2_time < 20:
-            # Your code here
-            
+
+            obstacle_coords.pose.position.x, obstacle_coords.pose.position.y = obstacle_coordinates(current_point[0],current_point[1],z_th,obstacle_length)
+            obstacle_pub.publish()
             set_point(x, y, z, np.pi/1000, np.pi/1000, z_th)   
 
         x_c = x
